c6c47c24de1ede7d02099d817a680f128bd45956
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2025-12-11 11:29:20 +0300

    add alx bookstore database schema

---
+++ alx_book_store.sql
@@ -0,0 +1,61 @@
+-- ==================================
+-- DATABASE: alx_book_store
+-- ==================================
+
+-- Create Database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+-- ====================================================
+-- TABLE: Authors
+-- ====================================================
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- ====================================================
+-- TABLE: Books
+-- ====================================================
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- ====================================================
+-- TABLE: Customers
+-- ====================================================
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+-- ====================================================
+-- TABLE: Orders
+-- ====================================================
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- ====================================================
+-- TABLE: Order_Details
+-- ====================================================
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+-- ============================= END =============================
